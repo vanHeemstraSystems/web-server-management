c17dc99f430feed5beea7ee84f272c2b94983c07
Willem van Heemstra <wvanheemstra@icloud.com>
2023-08-22 08:01:34 +0000

    Add Apollo Client

--- containers/app/plotly/src/app/page.tsx
+++ containers/app/plotly/src/app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+import "./page.scss";
 import Image from 'next/image'
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apollo";
+import { LinkWrapper } from "./linkWrapper";
 
-export default function Home() {
+export default function Home(pageProps:any) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
